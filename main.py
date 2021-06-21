274d8afcdc064f9de9ff497b67bb93f422321ef4
mayong <1552495348@qq.com>
2021-06-21 23:18:11 +0800

    docker 创建

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-if __name__ == '__main__':
-    uvicorn.run('main:app', host = '127.0.0.1',port = 8000, reload = True, debug = True, workers = 1)
+if __name__ == '__main__':`
+    uvicorn.run('main:app', host = '127.0.0.1',port = 8080, reload = True, debug = True, workers = 1)
